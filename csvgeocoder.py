7ee304fb3cc85789a4ada8921417d7a1d0746065
Bryce <bgust9@gmail.com>
2018-04-08 18:11:19 -0500

    Fixed exception error again - placed except directly after read_csv instead of at the end of the block

--- csvgeocoder.py
+++ csvgeocoder.py
@@ -14,21 +14,21 @@ def success():
     global filename
     if request.method=='POST':
         if '.csv' in request.files["file"].filename:
+            csvfile=request.files["file"]
             try:
-                csvfile=request.files["file"]
-                filename=secure_filename(csvfile.filename)
                 table=pandas.read_csv(csvfile, sep=',')
-                nom=Nominatim(scheme='https')
-                if table.columns.str.contains("Address" or "address").any()==True:
-                    table["Latitude"]=table["Address"or"address"].apply(nom.geocode).apply(lambda x: x.latitude if x != None else None)
-                    table["Longitude"]=table["Address"or"address"].apply(nom.geocode).apply(lambda x: x.longitude if x != None else None)
-                    table.to_csv("uploads/new"+filename, sep=',', index=False, encoding='utf-8')
-                    data=table.to_html(classes="table", index=False, index_names=False, justify='center', na_rep='--')
-                    return render_template("success.html", data=data, btn="download.html")
-                else:
-                    return render_template("index.html", message="There doesn't seem to be an Address (or address) column in your file")
             except:
                 return render_template("index.html", message="That file cannot be read as a CSV file")
+            filename=secure_filename(csvfile.filename)
+            nom=Nominatim(scheme='https')
+            if table.columns.str.contains("Address" or "address").any()==True:
+                table["Latitude"]=table["Address"or"address"].apply(nom.geocode).apply(lambda x: x.latitude if x != None else None)
+                table["Longitude"]=table["Address"or"address"].apply(nom.geocode).apply(lambda x: x.longitude if x != None else None)
+                table.to_csv("uploads/new"+filename, sep=',', index=False, encoding='utf-8')
+                data=table.to_html(classes="table", index=False, index_names=False, justify='center', na_rep='--')
+                return render_template("success.html", data=data, btn="download.html")
+            else:
+                return render_template("index.html", message="There doesn't seem to be an Address (or address) column in your file")
         else:
             return render_template("index.html", message="That doesn't appear to be a CSV file. Please select a CSV file")
 
