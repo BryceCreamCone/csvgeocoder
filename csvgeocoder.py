03c61a71167d120faf703a8a1d9d7f8b2847fc71
Bryce <bgust9@gmail.com>
2018-04-08 16:04:51 -0500

    first commit after deletion

---
+++ csvgeocoder.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, send_file
+import pandas
+from geopy.geocoders import Nominatim
+from werkzeug.utils import secure_filename
+
+app=Flask(__name__)
+nom=Nominatim(scheme='https')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success/", methods=["POST"])
+def success():
+    global filename
+    if request.method=='POST':
+        if '.csv' in request.files["file"].filename:
+            csvfile=request.files["file"]
+            filename=secure_filename(csvfile.filename)
+            table=pandas.read_csv(csvfile, sep=',')
+            if table.columns.str.contains("Address" or "address").any()==True:
+                table["Latitude"]=table["Address"or"address"].apply(nom.geocode).apply(lambda x: x.latitude if x != None else None)
+                table["Longitude"]=table["Address"or"address"].apply(nom.geocode).apply(lambda x: x.longitude if x != None else None)
+                table.to_csv("new"+filename, sep=',', index=False, encoding='utf-8')
+                data=table.to_html(classes="table", index=False, index_names=False, justify='center', na_rep='--')
+                return render_template("success.html", data=data, btn="download.html")
+            else:
+                return render_template("index.html", message="There doesn't seem to be an Address (or address) column in your file")
+        else:
+            return render_template("index.html", message="That doesn't appear to be a CSV file. Please select a CSV file")
+
+@app.route("/download/")
+def download():
+    return send_file("new"+filename, attachment_filename="new"+filename, as_attachment=True)
+
+if __name__=='__main__':
+    app.debug=True
+    app.run()
